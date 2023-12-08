58de72f9d6bb4b96c1bba47381ac424e87a9d45a
Julian Maas <jumaas@students.uni-mainz.de>
2023-12-08 12:32:16 +0100

    use Injection for Sensor in Alarm and add tests using mocks for Sensor

--- Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,26 +1,30 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
+public class Alarm {
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    protected Sensor sensor = new Sensor();
+    protected Sensor sensor;
 
     protected boolean alarmOn = false;
 
-    public void check()
-    {
+    public Alarm(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    private static boolean pressureExceedingThreshold(double psiPressureValue) {
+        return psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue;
+    }
+
+    public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue)
-        {
+        if (pressureExceedingThreshold(psiPressureValue)) {
             alarmOn = true;
         }
     }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
+    public boolean isAlarmOn() {
+        return alarmOn;
     }
 }
--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,78 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AlarmTest {
+    @Mock
+    Sensor sensor;
+    @InjectMocks
+    Alarm alarm;
 
-    @Test
-    public void alarmIsInitiallyOff() {
-        Alarm alarm = new Alarm();
-        assertThat(alarm.isAlarmOn()).isFalse();
+    @Nested
+    class SingleChecks {
+        @Test
+        public void alarmIsInitiallyOff() {
+            assertThat(alarm.isAlarmOn()).isFalse();
+        }
+
+        @Test
+        void checkActivatesAlarmWhenPressureIsTooLow() {
+            when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isTrue();
+        }
+
+        @Test
+        void checkActivatesAlarmWhenPressureIsTooHigh() {
+            when(sensor.popNextPressurePsiValue()).thenReturn(21.1);
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isTrue();
+        }
+
+        @ParameterizedTest
+        @ValueSource(doubles = {17, 18, 19, 20, 21})
+        void checkDoesNotActivateForPressureWithinThreshold(double pressure) {
+            when(sensor.popNextPressurePsiValue()).thenReturn(pressure);
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isFalse();
+        }
+    }
+
+    @Nested
+    class MultipleChecks {
+        @Test
+        void checkTwiceActivatesAlarmForPressureTooHighAfterPressureWithinThreshold() {
+            when(sensor.popNextPressurePsiValue()).thenReturn(21.0, 21.1);
+            alarm.check();
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isTrue();
+        }
+
+        @Test
+        void checkKeepsAlarmOnForPressureWithinThresholdAfterPressureTooLow() {
+            when(sensor.popNextPressurePsiValue()).thenReturn(16.0, 17.0);
+            alarm.check();
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isTrue();
+        }
+
+        @Test
+        void checkKeepsAlarmOnForPressureWithinThresholdAfterPressureTooHigh() {
+            when(sensor.popNextPressurePsiValue()).thenReturn(22.0, 21.0);
+            alarm.check();
+            alarm.check();
+            assertThat(alarm.isAlarmOn()).isTrue();
+        }
     }
 }
